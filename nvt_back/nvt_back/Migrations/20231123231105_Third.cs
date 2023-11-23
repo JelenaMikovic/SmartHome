9666914b1bbff09a5e97ef0d1cc3544d2b65a57f
Jelena Mikovic <jelena.mikovic189@gmail.com>
2023-11-24 00:33:52 +0100

    Merge fixes

---
+++ nvt_back/nvt_back/Migrations/20231123231105_Third.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace nvt_back.Migrations
+{
+    /// <inheritdoc />
+    public partial class Third : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "IsActivated", "Name", "Password", "Role", "Surname" },
+                values: new object[] { 2, "rob@mail.com", true, "Rob", "123", 0, "Boss" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
